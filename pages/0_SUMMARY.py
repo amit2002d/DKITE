8caa1e54efb85a2548523decf1eca71b1e600f9d
amit2002d <amitgulati78@gmail.com>
2024-07-16 10:05:51 +0530

    message

--- pages/0_SUMMARY.py
+++ pages/0_SUMMARY.py
@@ -151,7 +151,7 @@ while True:
             pnl = (cmp-buy_price)/buy_price if buy_price != 0 else 0
             multi_fac = -1*round(pnl*1000,2)
             if st.session_state.user == 'Amit' or st.session_state.user == "Deepti":
-                    amt = 30000
+                    amt = 35000
             else:
                 amt = 2500
             variable = 0
